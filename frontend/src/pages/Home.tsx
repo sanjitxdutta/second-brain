ca892d62e61b0ddc1e7adacffa76a4e43e69241a
sanjitxdutta <duttasanjit691@gmail.com>
2025-08-09 07:46:27 +0530

    Created the card component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,22 +1,16 @@
 import { FaShareAlt, FaPlus } from "react-icons/fa";
-import { Button } from "../components/Button";
+import Card from "../components/Card";
 
 export default function App() {
   return (
-    <div className="p-6 space-y-4">
-      <Button
-        variant="primary"
-        size="sm"
-        text="Add Content"
-        startIcon={<FaPlus />}
-        onClick={() => console.log("Add clicked")}
-      />
-
-      <Button
-        variant="secondary"
-        size="sm"
-        text="Next Step"
-        startIcon={<FaShareAlt />} 
+    <div className="p-6">
+      <Card
+        title="How to Build a Second Brain"
+        subtitle="YouTube Video"
+        link="https://sanjitxdutta.vercel.app/"
+        tags={["productivity", "learning"]}
+        dateAdded="2024-09-03"
+        onDelete={() => console.log("Deleted")}
       />
     </div>
   );
